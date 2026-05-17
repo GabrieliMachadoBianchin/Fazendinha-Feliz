599f0c06e4dca252dd3acf9a4aebaa42baf96053
Gabrieli <gbianchin@alunos.utfpr.edu.br>
2026-05-17 01:09:22 -0300

    fix movimento do player

--- Projeto/Assets/Scripts/PlayerController.cs
+++ Projeto/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 10f;
     public float gravity = -20f;
+    public float rotationSpeed = 10f;
 
     private CharacterController controller;
     private Vector3 velocity;
@@ -20,16 +21,34 @@ public class PlayerController : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
+/*
         Vector3 move = new Vector3(h, 0f, v);
 
         // Move no espaço do mundo
         controller.Move(move * speed * Time.deltaTime);
+*/
+
+        Vector3 move = transform.forward * v + transform.right * h;
+
+        //move = Vector3.ClampMagnitude(move, 1f);
+
+        controller.Move(move * speed * Time.deltaTime);
 
         // Rotaciona
+        /*
         if (move != Vector3.zero)
         {
             transform.forward = move;
+        }*/
+        if (move != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                targetRotation,
+                rotationSpeed * Time.deltaTime
+            );
         }
 
         // Gravidade
