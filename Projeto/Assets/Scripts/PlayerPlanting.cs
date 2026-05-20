20cc88081a2d6d84ca9672163e696ae4662ef686
Gabrieli <gbianchin@alunos.utfpr.edu.br>
2026-05-19 22:03:28 -0300

    Planta e colhe né

--- Projeto/Assets/Scripts/PlayerPlanting.cs
+++ Projeto/Assets/Scripts/PlayerPlanting.cs
@@ -10,6 +10,76 @@ public class PlayerPlanting : MonoBehaviour
 
     void Update()
     {
+        // Plantar
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TryPlant();
+        }
+
+        // Colher
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            TryHarvest();
+        }
+    }
+
+    void TryPlant()
+    {
+        FarmTile tile = GetTargetTile();
+
+        if (tile == null) return;
+
+        if (!tile.hasPlant)
+        {
+            tile.PlantSeed(seedPrefab);
+
+            Debug.Log("Plantou!");
+        }
+        else
+        {
+            Debug.Log("Já existe planta aqui.");
+        }
+    }
+
+    void TryHarvest()
+    {
+        FarmTile tile = GetTargetTile();
+
+        if (tile == null) return;
+
+        if (tile.hasPlant)
+        {
+            tile.HarvestPlant();
+        }
+    }
+
+    FarmTile GetTargetTile()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            // return hit.collider.GetComponent<FarmTile>();
+            return hit.collider.GetComponentInParent<FarmTile>();
+        }
+
+        return null;
+    }
+}
+/*using UnityEngine;
+
+public class PlayerPlanting : MonoBehaviour
+{
+    public Camera cam;
+
+    public float interactDistance = 5f;
+
+    public GameObject seedPrefab;
+
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -34,4 +104,4 @@ public class PlayerPlanting : MonoBehaviour
             }
         }
     }
-}
+}*/
